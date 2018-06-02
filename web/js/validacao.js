9f2c778859bbf39e3fa3e96d6efce4715317b906
shield5 <elsom.junr@gmail.com>
2018-06-02 20:04:42 -0300

    mudanças no formulario de contato (html, css, js)

--- web/js/validacao.js
+++ web/js/validacao.js
@@ -43,3 +43,21 @@ jQuery.validator.addMethod("validacaoCpf", function(value, element) {
     return this.optional(element) || retorno
 
 }, "Informe um CPF válido")
+
+/*
+ * Valida numeros de telefones brasileiros
+ */
+$.validator.addMethod("phoneBR", function (phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, "");
+    return this.optional(element) || phone_number.length > 9 &&
+            phone_number.match(/^(\(\d{2}\)\s?|\d{2}[\s.-]?)\d{4}[\s-]?\d{4}$/);
+}, "Por favor, digite um n&uacute;mero de telefone v&aacute;lido. Por exemplo: (99)9999-9999");
+
+/*
+ * Valida numeros de celular brasileiros
+ */
+$.validator.addMethod("mobileBR", function (phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, "");
+    return this.optional(element) || phone_number.length > 9 &&
+            phone_number.match(/^(\(\d{2}\)\s?|\d{2}[\s.-]?)\d{5}[\s-]?\d{4}$/);
+}, "Por favor, digite um n&uacute;mero de celular v&aacute;lido. Por exemplo: (99)99999-9999");
