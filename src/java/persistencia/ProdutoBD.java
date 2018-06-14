6ed799a0637cbd880c561295a476500fe905d802
CleiberReis <cleiberrodriguesreis@gmail.com>
2018-06-14 01:07:56 -0300

    Criada a Classe Java - "ProdutoBD.java" - Servirá de BD para salvar os produtos.

---
+++ src/java/persistencia/ProdutoBD.java
@@ -0,0 +1,105 @@
+package persistencia;
+
+import com.thoughtworks.xstream.XStream;
+import dominio.Produto;
+import java.io.File; // arquivo
+import java.io.FileWriter; //escritor de arquivo
+import java.util.ArrayList; // vetor
+
+public class ProdutoBD {
+
+    private static String caminho = "C:\\Users\\cleib\\Desktop\\Site_Nice_Decora-es\\";
+
+    private static ArrayList<Produto> lista = new ArrayList<Produto>();
+
+    //adiciona um objeto da classe Usuario 
+    //na lista que simula o banco de dados
+    public static void inserir(Produto produto) {
+        lerXml(); //Lê o XML e preenche a lista de usuario
+
+        //pega o último usuário cadastrado
+        int ultimaPosicao = lista.size() - 1;
+        if (ultimaPosicao >= 0) {
+            Produto ultimoProduto = lista.get(ultimaPosicao);
+            produto.setCodigo(ultimoProduto.getCodigo() + 1);
+        } else {
+            //Se não tem ninguém na lista o código é um.
+            produto.setCodigo(1);
+        }
+
+        lista.add(produto); // Adiciona um usuario no final da lista
+        salvarXml(); //Salva a lista atualizada com a adição do novo usuario
+    }
+
+    public static void alterar(Produto produto) {
+        lerXml();
+        excluir(produto.getCodigo());
+        inserir(produto);
+        salvarXml();
+    }
+
+    //recebe o atributo que identifica cada objeto
+    //da classe Usuario
+    public static void excluir(int codigo) {
+        lerXml();
+        for (int i = 0; i < lista.size(); i++) {
+            Produto cadaProduto = lista.get(i);
+
+            //procura o usuario que tem o CPF igual 
+            //Ã  variÃ¡vel "cpf", que tÃ¡ chegando entre
+            //os parÃªnteses
+            if (cadaProduto.getCodigo() == codigo) {
+                lista.remove(i);
+                /*.equals serve para comparar string */
+            }
+        }
+        salvarXml();
+    }
+
+    public static ArrayList<Produto> listar() {
+        lerXml();
+        //retorna todos os objetos do banco de dados
+        return lista;
+    }
+
+    public static Produto getBycodigo(int codigo) {
+        lerXml();
+        Produto produtoEncontrado = null;
+        for (int i = 0; i < lista.size(); i++) {
+            Produto cadaProduto = lista.get(i);
+
+            //procura o usuario que tem o CPF igual 
+            //Ã  variÃ¡vel "cpf", que tÃ¡ chegando entre
+            //os parÃªnteses
+            if (cadaProduto.getNome().equals(codigo)) {
+                produtoEncontrado = cadaProduto;
+                break;
+            }
+        }
+        return produtoEncontrado;
+    }
+
+    private static void lerXml() {
+        File arquivo = new File(caminho + "produtoos.xml");
+        if (arquivo.exists()) {
+            //armazenar XML no vetor
+            XStream xstream = new XStream();
+            xstream.alias("produto", Produto.class);
+            lista = (ArrayList<Produto>) xstream.fromXML(arquivo);
+        } else {
+            lista = new ArrayList<Produto>();
+        }
+    }
+
+    private static void salvarXml() {
+        XStream xstream = new XStream();
+        xstream.alias("produto", Produto.class);
+        try {
+            FileWriter escritor = new FileWriter(caminho + "produtoos.xml");
+            escritor.write(xstream.toXML(lista));
+            escritor.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
